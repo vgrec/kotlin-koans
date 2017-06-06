d9a0bfb44e445facab69846252580f240379372f
Veaceslav Grec <veaceslav.grec@virtual-solution.com>
2017-06-06 17:18:33 +0200

    complete Lambdas

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,30 @@
+import i_introduction._4_Lambdas.JavaCode4
+
 /**
  * Author vgrec created on 31.05.17.
  */
 
 fun main(args: Array<String>) {
-    println(test(4, 10, "30"))
+    higherOrderFunc(100, { println("Hello What") })
 
-    val list = listOf(1, 3, 4, 5)
-    println(list.joinToString(prefix = "{", postfix = "}"))
+
+    val sum = { x: Int, y: Int -> x + y }
+    println(sum(100, 100))
+
+    val sum2: (Int, Int) -> Int = { x, y -> x + y }
+    println(sum2(20, 20))
+
+    val test = JavaCode4()
+    val list = listOf(1, 2, 3)
+    println(test.task4(list))
+
 }
+
 
 fun test(a: Int, b: Int = 0, text: String = ""): Int {
     return a + b + text.toInt()
+}
+
+fun higherOrderFunc(a: Int, func: (text: String) -> Unit) {
+    func.invoke("Yes")
 }
